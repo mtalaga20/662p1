47773282bd49f5a5174a8dabdaa1d1cc2b971abf
mtalaga20 <michael.talaga@hotmail.com>
2021-09-26 12:43:30 -0500

    Final update

--- p1.hs
+++ p1.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE GADTs, FlexibleContexts #-}
 
+-- Michael Talaga, EECS 662, Project 1
+
 -- Imports for Monads
 
 import Control.Monad
@@ -27,52 +29,123 @@ data ABE where
 -- Evaluation Functions
 
 evalM :: ABE -> (Maybe ABE)
-evalM (Num x) = if x < 0 then Nothing else Just (Num x)
---evalM (Plus l r) = 
---evalM (Minus l r) = 
---evalM (Mult l r) = 
---evalM (Div l r) =
---evalM (Boolean b) = 
---evalM (And l r) = 
---evalM (Leq l r) = 
---evalM (Is0 l) = 
---evalM (If a b c) = 
+evalM (Num x) = if x < 0 then Nothing else return (Num x)
+evalM (Plus l r) = do {(Num l') <- (evalM l);
+                        (Num r') <- (evalM r);
+                        return (Num ( l' + r'))}
+evalM (Minus l r) = do {(Num l') <- (evalM l);
+                        (Num r') <- (evalM r);
+                        if l' - r' < 0 then Nothing else return (Num (l' - r'))}
+evalM (Mult l r) = do {(Num l') <- (evalM l);
+                        (Num r') <- (evalM r);
+                        return (Num (l' * r'))}
+evalM (Div l r) = do {(Num l') <- (evalM l);
+                        (Num r') <- (evalM r);
+                        if r' == 0 then Nothing else return (Num (l' `div` r'))}
+evalM (Boolean b) = return (Boolean b)
+evalM (And l r) = do {(Boolean l') <- (evalM l);
+                      (Boolean r') <- (evalM r);
+                      if l' == r' then return (Boolean True) else return (Boolean False)}
+evalM (Leq l r) = do {(Num l') <- (evalM l);
+                      (Num r') <- (evalM r);
+                      if l' <= r' then return (Boolean True) else return (Boolean False)}
+evalM (IsZero l) = do {(Num l') <- (evalM l);
+                    if l' == 0 then return (Boolean True) else return ( Boolean False)}
+evalM (If a b c) = do { (Boolean a') <- (evalM a);
+                        if a' then (evalM b) else (evalM c)}
 evalM _ = Nothing -- Replace this with your interpreter
 
 -- Type Derivation Function
 
 typeofM :: ABE -> Maybe TABE
-typeof(Num n) = if n >= 0 then return TNUM else Nothing
-typeof(BOOLEAN b) = return TBOOL
+typeofM (Num n) = if n >= 0 then return TNum else Nothing
+typeofM (Boolean b) = return TBool
 
-typeof(Plus l r) = do { TNUM <- typeof(l);
-			TNUM <- typeof(r);
-			return TNUM;}
-typeof(And l r) = do {TBOOL <- typeof(l);
-			TBOOL <- typeof(r);
-			return TBOOL}
---typeof(Minus l r) = do {
---typeof (Leq l r) = do {TNUM <- typeof(l);
-			TNUM <- typeof(r);
-			return TBOOL}
-typeof(Is0 l) = do {TNUM <- typeof(l);
-			return TBOOL}
-typeof(If a b c) = do {TBOOL <- typeof(a);
-			b' <- typeof(b);
-			c' <- typeof(c);
-			if b' == c' return b' else Nothing} --checking types of b' and c'
-
+typeofM (Plus l r) = do { TNum <- (typeofM l);
+                        TNum <- (typeofM r);
+                        return TNum}
+typeofM (And l r) = do {(TBool) <- (typeofM l);
+		  	              (TBool) <- (typeofM r);
+			                return TBool}
+typeofM (Minus l r) = do { TNum <- (typeofM l);
+                        TNum <- (typeofM r);
+                        return TNum}
+typeofM (Mult l r) = do { TNum <- (typeofM l);
+                        TNum <- (typeofM r);
+                        return TNum}
+typeofM (Div l r) = do { TNum <- (typeofM l);
+                        TNum <- (typeofM r);
+                        return TNum}
+typeofM (Leq l r) = do {TNum <- typeofM(l);
+			                TNum <- typeofM(r);
+			                return TBool}
+typeofM (IsZero l) = do {TNum <- (typeofM l);
+			                return TBool}
+typeofM (If a b c) = do {TBool <- (typeofM a);
+                      b' <- (typeofM b);
+                      c' <- (typeofM c);
+                      if b' == c' then return b' else Nothing} --checking types of b' and c'
 typeofM _ = Nothing
 
 -- Combined interpreter
 
 evalTypeM :: ABE -> Maybe ABE
+evalTypeM (Num n) = if typeofM(Num n) /= (Nothing) then evalM(Num n) else Nothing
+evalTypeM (Plus l r) = do {((TNum)) <- typeofM(l);
+                         ((TNum)) <- typeofM(r);
+                        a <- (evalM (Plus l r));
+                        return a}           
+evalTypeM (Minus l r) = do {((TNum)) <- typeofM(l);
+                         ((TNum)) <- typeofM(r);
+                        a <- (evalM (Minus l r));
+                        return a}  
+evalTypeM (Mult l r) = do {((TNum)) <- typeofM(l);
+                         ((TNum)) <- typeofM(r);
+                        a <- (evalM (Mult l r));
+                        return a} 
+evalTypeM (Div l r) = do {((TNum)) <- typeofM(l);
+                         ((TNum)) <- typeofM(r);
+                        a <- (evalM (Div l r));
+                        return a}      
+evalTypeM (Boolean b) = if typeofM(Boolean b) /= (Nothing) then evalM(Boolean b) else Nothing
+evalTypeM (And l r) = do {((TBool)) <- typeofM(l);
+                         ((TBool)) <- typeofM(r);
+                        a <- (evalM (And l r));
+                        return a}  
+evalTypeM (Leq l r) = do {((TNum)) <- typeofM(l);
+                         ((TNum)) <- typeofM(r);
+                        a <- (evalM (Leq l r));
+                        return a}  
+evalTypeM (IsZero l) = do {((TNum)) <- typeofM(l);
+                        a <- (evalM (IsZero l));
+                        return a}  
+evalTypeM (If a b c) = do {TBool <- (typeofM a);
+                          (Boolean a') <- (evalM a);
+                          if a' then (evalM b) else (evalM c)}
 evalTypeM _ = Nothing
 
 -- Optimizer
 
 optimize :: ABE -> ABE
 optimize e = e
+optimize (Num n) = (Num n)
+optimize (Boolean b) = (Boolean b)
+optimize (Plus l (Num 0)) = (optimize l)  
+optimize (Plus l r) = (Plus (optimize l)(optimize r))
+optimize (Minus l r) = (Minus (optimize l)(optimize r))
+optimize (Mult l r) = (Mult (optimize l)(optimize r))
+optimize (Div l r) = (Div (optimize l)(optimize r))
+optimize (Leq l r) = (Leq (optimize l)(optimize r))
+optimize (IsZero l) = (IsZero (optimize l))
+optimize (If (Boolean True) b c) = (optimize b) 
+optimize (If (Boolean False) b c) = (optimize c) 
+optimize (And l r) = (And (optimize l) (optimize r))
+
+
 
 evalOptM :: ABE -> Maybe ABE
+evalOptM x = evalM (optimize (x)) --evaluates optimized x (can also be done with the combined interpreter)
 evalOptM _ = Nothing
+
+
+
